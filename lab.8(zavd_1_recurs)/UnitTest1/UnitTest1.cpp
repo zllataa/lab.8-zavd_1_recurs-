fd8a69b0af2b3e7bec52a4bfa6cf3614ffb113a1
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2022-03-24 03:26:50 +0200

    Add files via upload

---
+++ lab.8(zavd_1_recurs)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab.8(zavd_1_recurs)/lab.8(zavd_1_recurs).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char A[] = "QSJHJKHKHSQGHJSQ";
+			int a = Include(A, "QS", 0, 0);
+
+			Assert::IsTrue;
+		}
+		
+	};
+}
---
+++ lab.8(zavd_1_recurs)/lab.8(zavd_1_recurs)/lab.8(zavd_1_recurs).cpp
@@ -0,0 +1,42 @@
+﻿
+//Дано літерний рядок, який містить послідовність символів s0, ..., sn, ... .
+//1. Вияснити, чи є серед цих символів пара сусідніх букв “SQ” або ”QS”.
+
+#include <iostream>
+using namespace std;
+
+bool Include(const char* s, const char* cs, int i, int j)
+{
+	bool b, r = true;
+	for (int i = 0; cs[i] != '\0'; i++)
+	{
+		b = false;
+		for (int j = 0; s[j] != '\0'; j++)
+			if (cs[i] == s[j])
+				return true;
+		if (s[i] != '\0')
+			return Include(s, cs, i + 1, j + 1);
+		else
+			return false;
+		r = r && b;
+	}
+	return r;
+}
+
+
+int main()
+{
+	char s[101];
+	cout << "Enter string:" << endl;
+	cin.getline(s, 100);
+	char cs[] = "SQ";
+	cout << "Included groups of 'SQ or QS': ";
+
+	if (Include(s, cs, 0, 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	
+	return 0;
+}
+
